f4927355fee9835b125f84d9355447e0355dff67
Piter30 <ditq65@gmail.com>
2024-11-27 22:56:11 +0100

    routing try fix p2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './components/App';
+import App from './components/App.jsx';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
