f05667103df253ed370603fef0fdb675162e0374
Piter30 <ditq65@gmail.com>
2024-11-27 23:24:05 +0100

    fix p4

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,10 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // <https://vitejs.dev/config/>
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     sourcemap: true,
-//   },
-// });
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// <https://vitejs.dev/config/>
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: './index.html',
-    },
+    sourcemap: true,
   },
 });
